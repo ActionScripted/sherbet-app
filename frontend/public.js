fb4765a147e3d2eeb16016fe9a1b78cef25cdcb6
Michael Thompson <actionscripted@gmail.com>
2020-09-09 22:08:26 -0400

    parent directory rename because I'm on macos and the .app fucks things up

--- frontend/public.js
+++ frontend/public.js
@@ -2,6 +2,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 import './public.scss';
 import App from 'Components/App';
@@ -10,5 +11,20 @@ import App from 'Components/App';
 const appContainer = document.getElementById('sherbet-app');
 const appData = {};
 const appProps = {};
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://sherbet.test:8000/graphql'
+});
+
+
+
+client
+  .query({
+    query: gql`
+      query AllUsers
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App data={appData} {...appProps} />, appContainer);
