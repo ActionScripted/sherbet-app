a0e20550e8378d839202e2a515b875197a06cbe4
Michael Thompson <actionscripted@gmail.com>
2021-04-06 00:27:28 -0400

    adding note

--- sherbet/models.py
+++ sherbet/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class HistoryMixin(models.Model):
+    """
+    Todo:
+        * Might make sense in its own app.
+    """
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     user_created = models.ForeignKey(
